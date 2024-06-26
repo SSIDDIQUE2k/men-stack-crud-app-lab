8162ffd8b781243af4db35884d1583f8cbfa6899
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-06-26 01:44:46 -0400

    Last added

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// GET /
+
+app.get('/', async (req, res) => {
+    res.render('index.ejs');
+    }   ); 
+
 app.listen(3000, () => {
     console.log('server started');
-    });
+    }); 
