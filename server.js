4fdc56c705b40d4a5a48599379ef306f08aec7a1
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-06-26 02:05:41 -0400

    Last added

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+//  dotenv is a zero-dependency module that 
+//loads environment variables from a .env file into process.env.
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected'); // Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
+    }  );
 
 // GET /
 
